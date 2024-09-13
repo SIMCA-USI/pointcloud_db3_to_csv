2123ed2ebf85a3d20e496f49141faf2f0731f8d5
Guillermo Sanchez Guitierrez-Cabello <47209078+guillermosanchezg@users.noreply.github.com>
2024-09-13 14:25:13 +0200

    Update extract_pointcloud.py

--- extract_pointcloud.py
+++ extract_pointcloud.py
@@ -7,38 +7,28 @@ from rosidl_runtime_py.utilities import get_message
 from rclpy.serialization import deserialize_message
 import argparse
 
-# Función para guardar los datos en CSV
 def save_to_csv(cloud, csv_filename):
-    # Obtener los nombres de los campos
     field_names = [field.name for field in cloud.fields]
     print("Campos disponibles:", field_names)
     
-    # Abrir archivo CSV
     with open(csv_filename, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
-        # Escribir cabecera
         csv_writer.writerow(field_names)
-        # Iterar sobre los puntos del PointCloud2
         for point in pc2.read_points(cloud, field_names=field_names, skip_nans=True):
             print(point)
             csv_writer.writerow(point)
 
-# Leer el rosbag
 def extract_pointclouds_from_bag(bag_filename, topic, csv_dir):
     reader = rosbag2_py.SequentialReader()
     storage_options = rosbag2_py.StorageOptions(uri=bag_filename, storage_id='sqlite3')
     converter_options = rosbag2_py.ConverterOptions(input_serialization_format='cdr', output_serialization_format='cdr')
     reader.open(storage_options, converter_options)
 
-    # Recorrer los mensajes
     while reader.has_next():
         (msg_topic, msg, t) = reader.read_next()
         if msg_topic == topic:
-            # Obtener el tipo de mensaje para el topic
             msg_type = get_message('sensor_msgs/msg/PointCloud2')
-            # Deserializar el mensaje
             deserialized_msg = deserialize_message(msg, msg_type)
-            # Guardar el mensaje deserializado
             save_to_csv(deserialized_msg, os.path.join(csv_dir, f"pointcloud_{t}.csv"))
 
 if __name__ == "__main__":
@@ -49,9 +39,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    # Crear el directorio si no existe
     os.makedirs(args.csv_dir, exist_ok=True)
 
-    # Llamar la función para extraer y convertir los PointClouds
     extract_pointclouds_from_bag(args.bag_filename, args.topic, args.csv_dir)
 
